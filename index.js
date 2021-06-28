bb42e8f4ceb4f9c2416b64fff92b21c8b9afb732
Eric Sanchez <ericfsod@gmail.com>
2021-06-28 17:58:05 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+const num3 = 100;
+const num4= 96;
+
+const mod = num3 % num4;
+
+const max = Math.max(1, 2, 3, 4, 10, 20);
